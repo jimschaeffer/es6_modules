a834fc29e46a7af1e5bc3503ea7ccf7037b59302
jimschaeffer <130175038+jimschaeffer@users.noreply.github.com>
2023-07-12 14:27:39 -0500

    project setup finished

---
+++ src/car.js
@@ -0,0 +1,10 @@
+const { ids } = require("webpack")
+
+export default class Car {
+    constructor() {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
---
+++ src/wishlist.js
@@ -0,0 +1,9 @@
+import Car from "/car.js";
+
+export default class WishList {
+    constructor() {
+        this.list = [];
+        this.nextId = 0;
+        console.log(nextID);
+    }
+}
